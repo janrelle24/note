c7c28e3c833ddf62622d166e3aac6af57aad7686
janrelle24 <lubianojanrelle@gmail.com>
2025-12-17 09:35:05 +0800

    create api on server.js that save, open,and list of notes

--- server/server.js
+++ server/server.js
@@ -7,4 +7,54 @@ const PORT = 3000;
 
 //middleware
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));               
+app.use(express.static(path.join(__dirname, "../public")));
+
+//create api save notes
+app.post("/api/save", (req, res) =>{
+    const { filename, content } = req.body;
+
+    if(!filename){
+        return res.status(400).json({ error: "Filename is required" });
+    }
+    const filePath = path.join(NOTES_DIR, filename);
+
+    fs.writeFile(filePath, content, "utf8", (err) =>{
+        if(err){
+            return res.status(500).json({ error: "Failed to save note" });
+        }
+        res.json({ message: "File saved successfully" });
+    });
+});
+//open notes
+app.get("/api/open/:filename", (req, res) =>{
+    const filePath = path.join(NOTES_DIR, req.params.filename);
+
+    fs.readFile(filePath, "utf8", (err, data) =>{
+        if(err){
+            return res.status(500).json({ error: "Failed to open note" });
+        }
+        res.json({ content: data });
+    });
+});
+//list notes
+app.get("api/list", (req, res) =>{
+    fs.readdir(NOTES_DIR, (err, files) =>{
+        if(err){
+            return res.status(500).json({ error: "Cannot read notes" });
+        }
+        res.json(files);
+    });
+});
+
+//folder where notes will store
+const NOTES_DIR = path.join(__dirname, "notes");
+
+// Create notes folder if not exists
+if (!fs.existsSync(NOTES_DIR)) {
+    fs.mkdirSync(NOTES_DIR);
+}
+
+//start server
+app.listen(PORT, () =>{
+    console.log(`Server running at http://localhost:${PORT}`);
+});
